48f701a0f8125d905e52e3f90d411774f1ddcf64
hsrmy <hsrmy@emradc.xyz>
2021-06-17 00:11:55 +0900

    versionのメトリクス表示

--- collector/version.go
+++ collector/version.go
@@ -3,22 +3,42 @@ package collector
 import (
 	"encoding/json"
 	"log"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+const namespace = "version"
+
+type versionCollector struct {
+	currentVersion *prometheus.Desc
+	latestVersion  *prometheus.Desc
+}
 
 type Version struct {
 	Current string `json:"current"`
 	Latest  string `json:"latest"`
 }
 
-func NewVersionCollector() Version {
-	api := newAPI()
-	body := fetch(&api, "version")
+func NewVersionCollector() *versionCollector {
+	return &versionCollector{
+		currentVersion: prometheus.NewDesc("current_version", "Current Version of mirakurun.", []string{"host", "version"}, nil),
+		latestVersion:  prometheus.NewDesc("latest_version", "Latest version of mirakurun.", []string{"host", "version"}, nil),
+	}
+}
 
+func (vc *versionCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- vc.currentVersion
+	ch <- vc.latestVersion
+}
+
+func (vc *versionCollector) Collect(ch chan<- prometheus.Metric) {
+	api := newAPI()
+	body := fetch(&api, namespace)
 	var version Version
 	if err := json.Unmarshal(body, &version); err != nil {
 		log.Fatal(err)
 	}
 
-	return version
-
+	ch <- prometheus.MustNewConstMetric(vc.currentVersion, prometheus.GaugeValue, 1, api.Host, version.Current)
+	ch <- prometheus.MustNewConstMetric(vc.latestVersion, prometheus.GaugeValue, 1, api.Host, version.Latest)
 }
--- main.go
+++ main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"net/http"
 
 	"mirakurun-exporter/collector"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	addr = flag.String("listen address", ":9100", "The Address to listen on for HTTP Requests.")
 )
 
 func main() {
-	status := collector.NewStatusCollector()
-	version := collector.NewVersionCollector()
-	fmt.Println(status)
-	fmt.Println(version)
+	flag.Parse()
+
+	// s := collector.NewStatusCollector()
+	v := collector.NewVersionCollector()
+
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(v)
+
+	http.HandleFunc("/", indexPage)
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+
+	log.Println("Listening on ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Mirakurun Exporter</title>
+		</head>
+		<body>
+			<h1>Mirukurn Exporter</h1>
+			<p><a href="/metrics">Metrics</a></p>
+		</body>
+		</html>
+	`))
 }
